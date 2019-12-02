650b26f468aa972edb3910fd383dfc2e701a43cd
Cole Arendt <cole.arendt@outlook.com>
2019-12-02 08:47:01 -0500

    tweak logic to make more robust(?)

--- app.R
+++ app.R
@@ -1,1 +1,8 @@
-shiny::runApp("./subdir")
+target_file <- "./subdir/app2.R"
+res <- source(target_file)
+
+if (inherits(res$value, "shiny.appobj")) {
+  res$value
+} else {
+  shiny::runApp("./subdir/app2.R")
+}
